bd4050eb0dc9b2f96ff7009eb7b8283ba3e3f147
Aaron Szymanski <aarons@qualtrics.com>
2022-09-03 12:49:08 -0600

    update

---
+++ install.sh
@@ -0,0 +1,18 @@
+prefix() {
+    awk '{ print "    ", $0 }'
+}
+
+echo "--Build---------------------------"
+./build.sh 2>&1 | prefix
+
+echo "--Copy to app loader--------------"
+cp adhdclock.app.js ~/repos/BangleApps/apps/adhdtimer/app.js 2>&1 | prefix
+
+echo "--Commit changes------------------"
+cd ~/repos/BangleApps
+git commit -a -m "Update adhdclock/app.js" 2>&1 | prefix
+
+echo "--Push changes-------------------"
+git push 2>&1 | prefix
+
+echo "--Done---------------------------"
