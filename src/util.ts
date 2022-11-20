78d072891265b0530417d487cf5e8706bc0c5bbf
Aaron Szymanski <aarons@qualtrics.com>
2022-11-20 15:15:02 -0700

    Add debug mode

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,19 @@
 export function zeroPad(n: number) : string {
     return ("0" + n.toString(10)).substr(-2, 1000);
 }
+
+export var DEBUG=false
+
+export function reportEvent(message: string) {
+    if(DEBUG) {
+        console.log(message);
+        // @ts-ignore
+        Terminal.println(message);
+
+        var file = require("Storage").open("adhdclock.eventlog","a");
+
+        var now = new Date();
+        var ts = now.getFullYear() + "-" + zeroPad(now.getMonth()+1) + "-" + zeroPad(now.getDate()) + " " + zeroPad(now.getHours()) + ":" + zeroPad(now.getMinutes()) + ":" + zeroPad(now.getSeconds());
+        file.write(ts + " " + message + "\n");
+    }
+}
